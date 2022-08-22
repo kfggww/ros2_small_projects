b81d9ebc9266261d1663d5c4fac97513883dab28
kfggww <kfggww@outlook.com>
2022-08-22 22:23:53 +0800

    Add world file with red trace, implement trajectory tracking car.

--- src/moving_car/launch/moving_car_launch.py
+++ src/moving_car/launch/moving_car_launch.py
@@ -11,7 +11,10 @@ def generate_launch_description():
         [FindPackageShare("moving_car"), "urdf/moving_car.urdf"]
     )
 
-    # TODO: 这里处理urdf文件的处理不够优雅
+    empty_world = PathJoinSubstitution(
+        [FindPackageShare("moving_car"), "worlds/empty.sdf"]
+    )
+
     urdf_preprocess = ExecuteProcess(
         cmd=["xacro ", model_urdf, " > /tmp/moving_car.urdf"],
         name="generate_urdf",
@@ -20,14 +23,22 @@ def generate_launch_description():
     )
 
     start_ign_gazebo = ExecuteProcess(
-        cmd=["ign gazebo empty.sdf"], name="ign_gazebo", shell=True
+        cmd=["ign gazebo ", empty_world], name="ign_gazebo", shell=True
     )
 
     spawn_urdf_model = ExecuteProcess(
         cmd=[
-            'ign service -s /world/empty/create --reqtype ignition.msgs.EntityFactory --reptype ignition.msgs.Boolean --timeout 1000 --req \'sdf_filename: "/tmp/moving_car.urdf", name: "mvcar"\''
+            'ign service -s /world/empty/create --reqtype ignition.msgs.EntityFactory --reptype ignition.msgs.Boolean --timeout 3000 --req \'sdf_filename: "/tmp/moving_car.urdf", name: "small_car"\''
         ],
         name="load_model_urdf",
+        shell=True,
+    )
+
+    set_model_pose = ExecuteProcess(
+        cmd=[
+            "ign service -s /world/empty/set_pose --reqtype ignition.msgs.Pose --reptype ignition.msgs.Boolean --timeout 3000 --req 'name: \"small_car\", position: {x: 0, y: 0, z: 0.03}, orientation: {x: 0, y: 0, z: 1, w: 0.8}'"
+        ],
+        name="set_model_pose",
         shell=True,
     )
 
@@ -43,12 +54,23 @@ def generate_launch_description():
         arguments=["/car_camera_raw@sensor_msgs/msg/Image[ignition.msgs.Image"],
     )
 
+    cmd_vel_topic_bridge = Node(
+        package="ros_ign_bridge",
+        executable="parameter_bridge",
+        arguments=["/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist"],
+    )
+
+    car_controller = Node(package="moving_car", executable="car_controller")
+
     return LaunchDescription(
         [
             urdf_preprocess,
             start_ign_gazebo,
             spawn_urdf_model,
+            set_model_pose,
             lidar_topic_bridge,
             camera_topic_bridge,
+            cmd_vel_topic_bridge,
+            car_controller,
         ]
     )
---
+++ src/moving_car/src/car_controller.cpp
@@ -0,0 +1,73 @@
+#include <cstring>
+#include <functional>
+#include <algorithm>
+#include <cmath>
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/subscription_options.hpp"
+#include "rclcpp/publisher_options.hpp"
+
+#include "cv_bridge/cv_bridge.h"
+#include "geometry_msgs/msg/twist.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+using std::placeholders::_1;
+
+class CarController : public rclcpp::Node
+{
+public:
+    CarController(const std::string &name) : Node(name)
+    {
+        camera_subs_ = this->create_subscription<sensor_msgs::msg::Image>("/car_camera_raw", 10, std::bind(&CarController::camera_image_callback, this, _1));
+        cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    }
+
+protected:
+    void camera_image_callback(const sensor_msgs::msg::Image &img)
+    {
+        const double target_center = img.width / 2.0;
+        const double kp = 0.1;
+
+        // 转换原始图像
+        auto cv_img = cv_bridge::toCvCopy(img, "bgr8");
+        cv::Mat gray_img;
+
+        // 灰度化
+        cv::cvtColor(cv_img->image, gray_img, cv::COLOR_BGR2GRAY);
+
+        // 二值化
+        cv::Mat bin_img;
+        cv::threshold(gray_img, bin_img, 128, 1, cv::THRESH_BINARY_INV);
+
+        // 计算图像矩
+        cv::Moments m = cv::moments(bin_img, true);
+        double center = m.m10 / m.m00;
+
+        // 更新速度控制指令
+        geometry_msgs::msg::Twist vel;
+        if (std::isfinite(center))
+        {
+            vel.linear.x = 0.5;
+            vel.angular.z = -kp * (center - target_center);
+        }
+
+        cmd_vel_pub_->publish(vel);
+    }
+
+private:
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_subs_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<CarController>("car_controller");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
