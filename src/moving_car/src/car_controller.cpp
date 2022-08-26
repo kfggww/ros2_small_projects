049f2255004a053f7f78e91bdbf23609c022f6db
kfggww <kfggww@outlook.com>
2022-08-26 22:09:16 +0800

    Add ros parameters support.

--- src/moving_car/src/car_controller.cpp
+++ src/moving_car/src/car_controller.cpp
@@ -24,6 +24,14 @@ public:
     {
         camera_subs_ = this->create_subscription<sensor_msgs::msg::Image>("/car_camera_raw", 10, std::bind(&CarController::camera_image_callback, this, _1));
         cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+        params_change_handler_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
+
+        auto callback = std::bind(&CarController::on_param_change, this, _1);
+        add_param_register_callback("kp1", 10.1, callback);
+        add_param_register_callback("kp2", 20.1, callback);
+        add_param_register_callback("v_normal", 0.3, callback);
+        add_param_register_callback("R", 0.08, callback);
+        add_param_register_callback("vline_ratio", 2.08, callback);
     }
 
 protected:
@@ -49,13 +57,14 @@ protected:
         int width = img.width;
         int height = img.height;
 
-        int vline_1 = width / 2.08;
-        int vline_2 = width - vline_1;
+        double kp1 = param_settings_map_["kp1"].param_val;
+        double kp2 = param_settings_map_["kp2"].param_val;
+        double v_normal = param_settings_map_["v_normal"].param_val;
+        double R = param_settings_map_["R"].param_val;
+        double vline_ratio = param_settings_map_["vline_ratio"].param_val;
 
-        const double v_normal = 0.3;
-        const double R = 0.08;
-        const double kp1 = 10.1;
-        const double kp2 = 20.1;
+        int vline_1 = width / vline_ratio;
+        int vline_2 = width - vline_1;
 
         double w = 0;
 
@@ -82,9 +91,43 @@ protected:
         cmd_vel_pub_->publish(vel);
     }
 
+    void on_param_change(const rclcpp::Parameter &param)
+    {
+        const std::string param_name = param.get_name();
+        double val = param.as_double();
+
+        ParamSetting &ps = param_settings_map_.at(param_name);
+        ps.param_val = val;
+
+        RCLCPP_INFO(this->get_logger(), "Set param [%s] to [%.3f]", param_name.c_str(), val);
+    }
+
+    void add_param_register_callback(const std::string &name, double value, rclcpp::ParameterEventHandler::ParameterCallbackType callback)
+    {
+
+        struct ParamSetting ps;
+        ps.param_name = name;
+        ps.param_val = value;
+
+        this->declare_parameter(name, value);
+        ps.param_cb_handle = params_change_handler_->add_parameter_callback(name, callback);
+
+        param_settings_map_.insert(std::make_pair(name, ps));
+    }
+
+private:
+    struct ParamSetting
+    {
+        std::string param_name;
+        double param_val;
+        std::shared_ptr<rclcpp::ParameterCallbackHandle> param_cb_handle;
+    };
+
 private:
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_subs_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    std::shared_ptr<rclcpp::ParameterEventHandler> params_change_handler_;
+    std::map<std::string, struct ParamSetting> param_settings_map_;
 };
 
 int main(int argc, char **argv)
